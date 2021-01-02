31136c28764c0e200ac69e95953cffa0d3cf6951
asgharlatif <asgharlatif@hotmail.com>
2021-01-02 12:43:59 -0500

    remove extra not required logic

--- src/Room.js
+++ src/Room.js
@@ -13,8 +13,8 @@ function Room() {
                     Current Room Temprature = {Temp}
             <br />
 
-            <button onClick={() => { if (isLit==false) setLit(true)}} >Turn Light On</button>            
-            <button onClick={() => { if (isLit==true) setLit(false)}} >Turn Light Off.</button>
+            <button onClick={() => setLit(true)} >Turn Light On</button>            
+            <button onClick={() => setLit(false)} >Turn Light Off.</button>
             <button onClick={() => setTemp(++Temp)}  >Increase Temprature</button>
             <button onClick={() => setTemp(--Temp)}  >Decrease Temprature</button>
         </div>
